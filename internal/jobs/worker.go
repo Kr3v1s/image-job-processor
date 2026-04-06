82ca64840348fe061069debb1c5f5b868cd5e3e1
kr3v1s <kevinbustamante67@gmail.com>
2026-04-07 02:11:05 +0900

    implement async job queue with worker pool and image download

--- internal/jobs/worker.go
+++ internal/jobs/worker.go
@@ -1,8 +1,11 @@
 package jobs
 
 import (
+	"io"
 	"log"
-	"time"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func StartWorker(id int) {
@@ -11,10 +14,43 @@ func StartWorker(id int) {
 
 		job.Status = "running"
 
-		// simula processamento pesado
-		time.Sleep(2 * time.Second)
+		resp, err := http.Get(job.URL)
+		if err != nil {
+			job.Status = "failed"
+			job.Error = err.Error()
+			log.Println("Worker", id, "failed job", job.ID, err)
+			continue
+		}
 
-		job.Result = "processed: " + job.URL
+		if resp.StatusCode != http.StatusOK {
+			job.Status = "failed"
+			job.Error = "failed to download image: " + resp.Status
+			resp.Body.Close()
+			log.Println("Worker", id, "failed job", job.ID, job.Error)
+			continue
+		}
+
+		filename := filepath.Join("output", job.ID+".img")
+		file, err := os.Create(filename)
+		if err != nil {
+			job.Status = "failed"
+			job.Error = err.Error()
+			resp.Body.Close()
+			log.Println("Worker", id, "failed job", job.ID, err)
+			continue
+		}
+
+		_, err = io.Copy(file, resp.Body)
+		resp.Body.Close()
+		file.Close()
+		if err != nil {
+			job.Status = "failed"
+			job.Error = err.Error()
+			log.Println("Worker", id, "failed job", job.ID, err)
+			continue
+		}
+
+		job.Result = filename
 		job.Status = "done"
 
 		log.Println("Worker", id, "finished job", job.ID)
